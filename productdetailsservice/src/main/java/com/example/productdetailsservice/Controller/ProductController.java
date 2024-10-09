1c4067e7b0f06b3a1f8d06566e5dc330eb6f5f20
Simphiwe-Sithebe <www.simz88@gmail.com>
2024-10-09 15:36:43 +0200

    WELP

--- productdetailsservice/src/main/java/com/example/productdetailsservice/Controller/ProductController.java
+++ productdetailsservice/src/main/java/com/example/productdetailsservice/Controller/ProductController.java
@@ -2,21 +2,26 @@ package main.java.com.example.productdetailsservice.Controller;
 
 import java.util.ArrayList;
 
+import main.java.com.example.productdetailsservice.Service.ProductService;
+
 @RestController
 @RequestMapping("/amazon/products")
 public class ProductController {
-    
+    @Autowired
+    ProductService productService;
+
     @PostMapping("/save")
     public Product saveData(@RequestBody Product product){
-
+        return productService.saveDataToDB(product);
     }
 
     @GetMapping("/getAllProducts")
     public ArrayList<Product> getAllProducts(){
-        
+        return productService.findAll();
     }
+    
     @GetMapping("/search/{productId}")
     public Product search(@PathVariable UUID productId){
-        
+        return productService.findAllByuniqueId(productId);
     }
 }
